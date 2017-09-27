3d9209a3d246a210364adf2a31d21241296caece
Damon V. Caskey <dc@caskeys.com>
2017-09-27 15:10:10 -0400

    Add Kanga library.
    Add Kanga palette control library and test with Hanzou.

--- data/chars/hanzou/spawn.c
+++ data/chars/hanzou/spawn.c
@@ -1,5 +1,13 @@
+#include "data/scripts/dc_kanga/main.c"
+
 void main()
 {
+
+    void self = getlocalvar("self");
+
+    setlocalvar(DC_KANGA_KEY_TARGET, self);
+
+    dc_kanga_random_index();
 
 }
 
---
+++ data/scripts/dc_kanga/main.c
@@ -0,0 +1,9 @@
+// Configuration.
+#include "data/scripts/dc_kanga/config.h"
+
+// Methods.
+#import "data/scripts/dc_kanga/increment_global_index.c"
+#import "data/scripts/dc_kanga/random_index.c"
+#import "data/scripts/dc_kanga/rotate_index.c"
+
+

bb30c6c57b84115d961b1804aff7ba402ffa9710
Damon V. Caskey <dc@caskeys.com>
2017-10-19 16:24:35 -0400

    Afterimage Progress
    Add constants and restore module's video size.

--- data/scripts/trailer.h
+++ data/scripts/trailer.h
@@ -1,3 +1,17 @@
+#ifndef DC_AFTERIMAGE
+
+#define DC_AFTERIMAGE
+
+#define DC_AFTERIMAGE_KEY_SPRITE        0
+#define DC_AFTERIMAGE_KEY_POS_X         1
+#define DC_AFTERIMAGE_KEY_POS_Y         2
+#define DC_AFTERIMAGE_KEY_POS_Z         3
+#define DC_AFTERIMAGE_KEY_POS_DIRECTION 4   // Direction
+#define DC_AFTERIMAGE_KEY_POS_CD        5   // Tracker. To be removed.
+
+#endif
+
+
 int trailermax = 50; //total trailers the engine can handle
 int trailerc = 8; // totally 5 trailer images for each character
 int trailerd = 7; // delay to spawn next trailer, so trailer lasts for trailermax*trailerd
